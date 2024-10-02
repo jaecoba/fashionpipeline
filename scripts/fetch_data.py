58b6ce65c1de398129111a1604cff4bb75fc0e74
jaec <jaecoba7@gmail.com>
2024-10-01 21:45:05 -0700

    initial commit, testing how many requests before being rate limited

---
+++ scripts/fetch_data.py
@@ -0,0 +1,19 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
+
+def fetch_trends(keywords):
+
+    pytrends = TrendReq(hl='US', tz=360)
+
+    pytrends.build_payload(keywords, cat=0, timeframe='today 1-m', geo='US', gprop='')
+
+    data = pytrends.interest_over_time()
+
+    return data
+
+if __name__ == "__main__":
+    #'cargo pants', 'dress pants', 'blue jeans', 'black jeans', 
+    keywords = ['dress pants','straight jeans', 'baggy jeans', 'bootcut jeans']
+    trend_data = fetch_trends(keywords)
+    trend_data.to_csv('data/trendData.csv', index=False)
